66c6aba5281956e3a25cd54e4ac87204785b085a
julbean <138324323+julbean@users.noreply.github.com>
2026-02-05 22:10:09 -0800

    Update describealign.py
    bump version

--- describealign.py
+++ describealign.py
@@ -1,4 +1,4 @@
-__version__ = '2.0.3'
+__version__ = '2.0.4'
 
 # combines videos with matching audio files (e.g. audio descriptions)
 # input: video or folder of videos and an audio file or folder of audio files
